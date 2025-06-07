b254e070be2e54c85782197109aab58177665523
Luis Augusto <gutopprado@gmail.com>
2025-06-07 08:57:01 -0300

    fix RL_infer

--- RL_infer.py
+++ RL_infer.py
@@ -7,17 +7,68 @@ from rSoccer import SSLMultiAgentEnv
 
 from scripts import InferenceModel
 from scripts import InferenceBetaDist
+# import debugpy
 
+# debugpy.listen(("0.0.0.0", 5679))
+# input("Aguardando o debugger...")
 # Daqui para baixo não precisa mexer
 
-def state_to_observation(state: dict[str, list], last_actions: dict[str, list], obs_space_size: int, n_steps: int, env_config: dict) -> dict[str, np.ndarray]:
+def state_to_observation(state: dict[str, list], last_actions: dict[str, list], obs_space_size: int, n_steps: int) -> dict[str, np.ndarray]:
+    """
+    Função que transforma o estado do ambiente em observações
     
+    :Params: *state* estado do ambiente
+        Exemplo:
+            state = {
+                "blue_0": [x, y, theta],
+                "blue_1": [x, y, theta],
+                "blue_2": [x, y, theta],
+                "yellow_0": [x, y, theta],
+                "yellow_1": [x, y, theta],
+                "yellow_2": [x, y, theta],
+                "ball": [x, y]
+            }
+    :Params: *last_actions* ações anteriores
+        Exemplo:
+            last_actions = {
+                "blue_0": [0, 0, 0, 0],
+                "blue_1": [0, 0, 0, 0],
+                "blue_2": [0, 0, 0, 0],
+                "yellow_0": [0, 0, 0, 0],
+                "yellow_1": [0, 0, 0, 0],
+                "yellow_2": [0, 0, 0, 0]
+            }
+    :Params: *obs_space_size* tamanho do espaço de observações
+    :Params: *n_steps* número de passos
+    :Params: *env_config* configuração do ambiente
+        Exemplo:
+            env_config = {
+                "init_pos": {
+                    "blue": [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                    "yellow": [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                    "ball": [0, 0]
+                },
+                "other_config": ...
+
+    
+    """
+    env_config = {
+        "init_pos": {
+            "blue": {}, 
+            "yellow": {}, 
+            "ball": {}
+        },
+        "field_type": 1,
+        "fps": 30, # frames por segundo
+        "match_time": 40, # duração da partida em segundos
+        "render_mode": "human"
+    }
     # Configura ambiente para receber o estado inicial com as posições dos robôs
     for robot_name in state.keys():
         if robot_name == "ball": continue
         team, robot_id = robot_name.split("_")
         robot_id = int(robot_id)
-        env_config["init_pos"][team][robot_id] = state[robot_name].copy()
+        env_config["init_pos"][team][robot_id+1] = state[robot_name].copy()
     env_config["init_pos"]["ball"] = state["ball"].copy()
 
     # Uso o ambiente aqui só para calcular as observações
@@ -57,7 +108,7 @@ def buffer(stack_observations: dict[str, np.ndarray], observation:  np.ndarray, 
 
     for robot_name in observation.keys():
         assert stack_observations[robot_name].shape == (n_stack * obs_space_size,), f"stack_observations[{robot_name}].shape: {stack_observations[robot_name].shape} != ({n_stack * obs_space_size},)"
-        assert state[robot_name].shape == (obs_space_size,), f"state[{robot_name}].shape: {state[robot_name].shape} != ({obs_space_size},)"
+        assert observation[robot_name].shape == (obs_space_size,), f"state[{robot_name}].shape: {observation[robot_name].shape} != ({obs_space_size},)"
 
         if robot_name == "ball": continue
         stack_observations[robot_name] = np.delete(stack_observations[robot_name], range(obs_space_size))
@@ -120,12 +171,15 @@ def policy(stack_observations: dict[str, np.ndarray], model: InferenceModel, tea
     actions = {}
     for i, robot_name in enumerate(stack_observations.keys()):
         if team not in robot_name: continue
-        actions[robot_name] = list(dist_output[i])
+        _, idx = robot_name.split("_")
+        idx = int(idx)
+        actions[robot_name] = list(dist_output[idx])
 
     return actions
 
 
 if __name__ == "__main__":
+    # ---------------------EXEMPLO DE USO---------------------
     with open("config.yaml") as f:
         file_configs: dict = yaml.safe_load(f)
         
@@ -139,7 +193,7 @@ if __name__ == "__main__":
         **{f"yellow_{i}": [0, 0, 0, 0] for i in range(3)}
     }
 
-    # Essa parte é só para testar----------------------------------
+    # Essa parte é só para esse teste pra construir state, não estaria no uso final----
     ENV = SSLMultiAgentEnv(**file_configs["env"])
     ENV.reset()
     state = {}
@@ -152,37 +206,68 @@ if __name__ == "__main__":
         state[robot_name] = ENV.observations[robot_name][-77:].copy()
     ball = ENV.frame.ball
     state["ball"] = [ball.x, ball.y]
-    #---------------------------------------------------------------
+    #---------------------------------------------------------------------------------
 
+    # observation = state_to_observation(
+    #     state=state, 
+    #     last_actions=last_actions,
+    #     obs_space_size=77,
+    #     n_steps=0,
+    # )
 
-
+    
     model = load_model(
-        checkpoint_path="/root/ray_results/PPO_selfplay_rec/PPO_Soccer_28842_00000_0_2024-12-06_02-52-40/checkpoint_000007",
+        checkpoint_path="/root/ray_results/PPO_selfplay_rec/PPO_Soccer_baseline_2025-03-16/checkpoint_000003",
         action_space_size=4,
         obs_space_size=77,
         n_stack=8
     )
 
-    observation = state_to_observation(
-        state=state, 
-        last_actions=last_actions,
-        obs_space_size=77,
-        n_steps=100,
-        env_config=file_configs["env"]
-    )
+    done = False
+    s = 0
+    while not done:
+        print(f"{f'step {s}':-^20}")
 
-    stacked_obs = buffer(
-        stack_observations=stacked_obs,
-        observation=observation,
-        obs_space_size=77,
-        n_stack=8
-    )
+        observation = state_to_observation(
+            state=state, 
+            last_actions=last_actions,
+            obs_space_size=77,
+            n_steps=s,
+        )
 
-    action = policy(
-        stack_observations=stacked_obs,
-        model=model,
-        team="blue"
-    )
+        stacked_obs = buffer(
+            stack_observations=stacked_obs,
+            observation=observation,
+            obs_space_size=77,
+            n_stack=8
+        )
+
+        #print("\n".join([f"robot {i+1}: {ENV._friendly_observation(stacked_obs['blue_0'][77*i:77*(1+i)])['pos']['robot']}" for i in range(8)]))
+
+        action = policy(
+            stack_observations=stacked_obs,
+            model=model,
+            team="blue"
+        )
+        last_actions.update(action)
+
+        obs, _, status, *_ = ENV.step(last_actions)
+        state = {
+            "blue_0": [ENV.frame.robots_blue[0].x, ENV.frame.robots_blue[0].y, ENV.frame.robots_blue[0].theta],
+            "blue_1": [ENV.frame.robots_blue[1].x, ENV.frame.robots_blue[1].y, ENV.frame.robots_blue[1].theta],
+            "blue_2": [ENV.frame.robots_blue[2].x, ENV.frame.robots_blue[2].y, ENV.frame.robots_blue[2].theta],
+            "yellow_0": [ENV.frame.robots_yellow[0].x, ENV.frame.robots_yellow[0].y, ENV.frame.robots_yellow[0].theta],
+            "yellow_1": [ENV.frame.robots_yellow[1].x, ENV.frame.robots_yellow[1].y, ENV.frame.robots_yellow[1].theta],
+            "yellow_2": [ENV.frame.robots_yellow[2].x, ENV.frame.robots_yellow[2].y, ENV.frame.robots_yellow[2].theta],
+            "ball": [ENV.frame.ball.x, ENV.frame.ball.y]
+        }
+
+        # observation = {team: obs[team][-77:].copy() for team in obs}
+        done = status["__all__"]
+        s += 1
+        ENV.render()
+
+        #input()
 
     print(action)
 
