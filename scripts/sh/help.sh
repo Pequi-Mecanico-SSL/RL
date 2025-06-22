b8067e48fe26012f6719d45c1f976322213dc8d4
Luis Augusto <gutopprado@gmail.com>
2025-06-21 22:07:33 -0300

    .

--- scripts/sh/help.sh
+++ scripts/sh/help.sh
@@ -5,4 +5,7 @@ docker run --gpus all --name pequi-ssl \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd)/volumes/videos:/ws/videos \
     -v $(pwd)/volumes/dgx_checkpoints/PPO_selfplay_rec:/root/ray_results/PPO_selfplay_rec \
+    -v $(pwd)/RL_train.py \
+    -v $(pwd)/RL_eval.py \
+    -v $(pwd)/RL_infer.py \
     -it ssl-el
